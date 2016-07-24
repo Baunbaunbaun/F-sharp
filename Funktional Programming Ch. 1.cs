a8fc7c6fcccd3b7861da1951732f0bed58e05ee6
Christian Baun <baunbaun@gmail.com>
2016-07-24 16:13:47 +0200

    Init

---
+++ Funktional Programming Ch. 1.cs
@@ -0,0 +1,45 @@
+
+Answers Ch. 1
+
+//ex 1.1
+let rec g n = 
+    n + 4.0;;
+
+//ex 1.2
+let rec h (x,y) : float =
+    System.Math.Sqrt(x*x + y*y);; 
+
+//ex 1.4
+let rec sum = function
+    | 0 -> 0
+    | n -> n + sum (n-1);;
+
+sum 4;;
+
+//ex 1.5
+let rec fibo = function
+    | 0 -> 0
+    | 1 -> 1
+    | n -> fibo (n-1) + fibo (n-2);;
+
+fibo 4;;
+
+//ex 1.6
+let rec sum = function
+    | (m,0) -> m
+    | (0,n) -> 0
+    | (m,n) -> m + (n+sum(m,n-1))
+
+//ex 1.7
+
+float * int -> float
+int -> int
+function (float * int) -> float
+float * int -> float
+
+//ex 1.8
+
+env = [
+    a -> 5
+    f(a) -> 6
+    g(b) -> 11
