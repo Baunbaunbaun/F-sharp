6b561adbc5d514b31acb2d68809139618e9d106b
Christian Baun <baunbaun@gmail.com>
2016-07-25 15:53:31 +0200

    ex 2.1-2.3

--- Answers Chapter 2.cs
+++ Answers Chapter 2.cs
@@ -1,6 +1,7 @@
 //Answers Ch. 2
 
 //ex 2.1
+
 let bol n = 
     (n % 2 = 0 || n % 3 = 0) && n % 5 = 0;;
 
@@ -16,6 +17,18 @@ let c = bol 29;;
 //true expected
 let d = bol 30;; 
 
-//ex 2.2
+//ex 2.21
+
 let pow (str:string,n) =
     str.[0..(n-1)]
+
+pow ("christian",3);;
+
+//ex 2.3
+//Why do I need to typecast the string input?
+
+let isIthChar (str:string,i,ch) =
+    str.[i] = ch;;
+
+isIthChar ("Chr",1,'h');;
+
