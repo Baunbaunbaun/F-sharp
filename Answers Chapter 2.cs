35fba5eb38adca2db0aac92ab8c387aa98c438e6
Christian Baun <baunbaun@gmail.com>
2016-07-25 09:37:24 +0200

    final hand in

---
+++ Answers Chapter 2.cs
@@ -0,0 +1,20 @@
+//Answers Ch. 2
+
+//ex 2.1
+let bol n = 
+    (n % 2 = 0 || n % 3 = 0) && n % 5 = 0
+
+//false expected
+let a = bol 24;;
+
+//false expected
+let b = bol 27;; 
+
+//false expected
+let c = bol 29;; 
+
+//true expected
+let d = bol 30;; 
+
+//ex 2.2
+let conc (s,n) =
